22747c92dc55369fb8f7d7e22cf637164cd3cb02
butt-h4ad <31614065+butt-h4ad@users.noreply.github.com>
2017-09-04 14:54:04 +0300

    Update APIpoloniex.py

--- APIpoloniex.py
+++ APIpoloniex.py
@@ -1,41 +1,69 @@
+#In English
+# # # The APIpoloniex class works with the API https://poloniex.com/ #
+#
+# This class has an error handler for poloniex requests, thanks to this
+# Your trading terminal will not hang for a long time.
+# #
+# If the query data, poloniex.com does not respond more timeOutSec seconds,
+# then the function returns -1 and an error message is displayed (APIerror)
+# #
+# The script is written for yourself, for trading on the stock exchange. Distributed without a license
+#
+#Для Русских
+# #			# Класс APIpoloniex работает с API https://poloniex.com/ #
+#
+# В данный клас встроен обработчик ошибок на запросы к poloniex, благодаря этому
+# Ваш торговый терминал не будет зависать на длительное время.
+# # 
+# Если на запрос данных, poloniex.com не отвечает больше timeOutSec секунд, 
+# то функция возвращает -1 и выводится сообщение об ошибке (APIerror)
+# #
+# Скрипт написан для себя, для торговли на бирже. Распростроняется без лицензии
+#
+# #			# For Example #
+#
+# from APIpoloniex import *
+# polo = APIpoloniex('api_key', 'api_secret',1.0)
+# print (polo.returnBalances())
+# #
+
+
 import requests
 from urllib.parse import urlencode 
-
 import json 
 from hmac import new 
 from hashlib import sha512 
-
 import sys
 import time
 
 class APIpoloniex(object):	
 #
-	def __init__(self,APIKey,Secret,timeout_sec=3.0):
+	def __init__(self,APIKey,secret,timeOutSec=3.0):
 		self.APIKey=APIKey
-		self.Secret=Secret
-		self.timeout_sec= timeout_sec
+		self.secret=secret
+		self.timeOutSec= timeOutSec
 		# set time labels
 		self.MINUTE, self.HOUR, self.DAY = 60, 60 * 60, 60 * 60 * 24
 		self.WEEK, self.MONTH = self.DAY * 7, self.DAY * 30
 		self.YEAR = self.DAY * 365
 		return
 
-#APIerror ВЫвод ошибок	
+#APIerror Вывод ошибок	
 	def APIerror(self,command, info):
 		print( "\n"+time.strftime("[%H:%M:%S]")+" Error while executing the command: " + "\"" +command + "\"")
 		print(time.strftime("[%H:%M:%S]"),info)
 		return
 
 #CommandPublic для публичных команд
-	def CommandPublic (self, command, args={}, timeout_sec=False):
-		if timeout_sec == False : timeout_sec= self.timeout_sec
+	def CommandPublic (self, command, args={}, timeOutSec=False):
+		if timeOutSec == False : timeOutSec= self.timeOutSec
 		url = 'https://poloniex.com/public?command=' + command
 		if args.get("currencyPair"): url= url +"&currencyPair=" + args['currencyPair']			
 		if args.get("period"): url= url +"&period=" + args['period']	
 		if args.get("start"): url= url +"&start=" + args['start']	
 		if args.get("end"): url= url +"&end=" + args['end']	
 		try:
-			html = requests.get(url,timeout=(timeout_sec,timeout_sec))
+			html = requests.get(url,timeout=(timeOutSec,timeOutSec))
 		except:
 			self.APIerror(command,sys.exc_info()[1])
 			return -1
@@ -43,13 +71,13 @@ class APIpoloniex(object):
 			return html.json()
 
 #CommandPrivate для приватных команд
-	def CommandPrivate(self,command="",args={},timeout_sec=False): #
-		if timeout_sec == False : timeout_sec= self.timeout_sec
+	def CommandPrivate(self,command="",args={},timeOutSec=False): #
+		if timeOutSec == False : timeOutSec= self.timeOutSec
 		args['command'] = command
 		args['nonce'] =  int(time.time()*1000000)		
 		url = "https://poloniex.com/tradingApi"
 		sign = new(
-                self.Secret.encode('utf-8'),
+                self.secret.encode('utf-8'),
                 urlencode(args).encode('utf-8'),
                 sha512).hexdigest()
 		headers = {
@@ -57,7 +85,7 @@ class APIpoloniex(object):
 		'Sign' :sign,
 		}    
 		try:
-			html = requests.post(url, data=args, headers=headers,timeout=(timeout_sec, timeout_sec))
+			html = requests.post(url, data=args, headers=headers,timeout=(timeOutSec, timeOutSec))
 		except:
 			self.APIerror(command,sys.exc_info()[1])
 			return -1	
@@ -85,14 +113,14 @@ class APIpoloniex(object):
 		args = { 'currencyPair': currencyPair }
 		return self.CommandPrivate('returnOpenOrders',args)
 
-#returnBalances Показывает баланс
+#returnBalances Возвращает баланс
 	def returnBalances(self):		
 		return self.CommandPrivate('returnBalances')
 
-#returnTradeHistory показывает историю моих сделок	
+#returnTradeHistory возвращает историю моих сделок	
 	def returnTradeHistory(self,currencyPair='all', start=False, end=False):
-		if start == 0 : start = time.time()- self.DAY
-		if end == 0 : end = time.time()
+		if start == False : start = time.time()- self.DAY
+		if end == False : end = time.time()
 		args = {'currencyPair':currencyPair,
 				'start'	: start,
 				'end'	: end
