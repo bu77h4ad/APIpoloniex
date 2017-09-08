ac1be0322c59efba47978a935b8effdaa513af46
buttHead <31614065+butt-h4ad@users.noreply.github.com>
2017-09-08 23:23:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
 
 setup(
     name='APIpoloniex',
     author='Butt-head',
-    version='1.0',
+    version='1.0.0',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    
 )
